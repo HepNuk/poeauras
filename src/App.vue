1008d56a4c3a84df76ccbcfd3a3cd3d707acdc64
nikola.perotic.1 <nikola.perotic.1@ens.etsmtl.ca>
2021-01-16 02:33:21 -0500

    Basic Styling Started

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Home msg="Nuk's Aura Stat Calc"/>
+    <div class="header">
+      <h1>Nuk's PoE Aura stats calculator</h1>
+      <p>- Under Construction</p>
+    </div>
+    <div class="content">
+        <TopNav />
+      <div class="inputs">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <Home msg="Nuk's Aura Stat Calc"/>
+      </div>
+      <div class="outputs">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import TopNav from './components/TopNav.vue'
 import Home from './views/Home.vue'
 
 export default {
   name: 'App',
   components: {
     Home,
+    TopNav,
   }
 }
 </script>
@@ -22,13 +36,40 @@ export default {
     src: url(./assets/fonts/Fontin-Bold.ttf);
 }
 
-.app {
-  
-  font-family: itemFont;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  padding: 10px;
+  background-color: rgb(29, 26, 26);
 }
+
+.app {
+  font-family: itemFont;
+  background-color: none;
+  padding: none;
+}
+
+.content {
+  padding: 0px;
+  background: red;
+}
+
+
+.header {
+  padding: 15px;
+  text-align: left;
+  background-color: none;
+  margin-bottom: -20px;
+  margin-top: -40px;
+}
+
+.header h1 {
+  color: rgb(212, 212, 212);
+  font-size: 40px;
+  margin-bottom: -15px;
+}
+
+.header p {  
+    font-size: larger;
+    color: white;
+}
+
 </style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="topnav">
+    <button>
+      Home
+    </button>
+    <button>
+      Asc
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.topnav {
+  overflow: hidden;
+  background-color: blue;
+  padding: 5px;
+  margin-bottom: -16px;
+}
+
+.topnav button {
+  background-color: chocolate;
+  /* Green */
+  color: black;
+  border-style: solid;
+  border-color: #c4580b;
+  border-radius: 5px;
+  padding: 10px 10px;
+  width: 150px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  
+}
+.topnav button:hover {
+  background-color: #4499a8;
+}
+
+.topnav button:active {
+  background-color: black;
+}
+</style>
